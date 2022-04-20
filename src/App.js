c87f91ad4ec0fa99ac8ccd1a4e51b8b134549e85
leedawnn <2g_hee@naver.com>
2022-04-20 22:44:11 +0900

    style: Apply styled-reset

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
 import Toggle from './components/Toggle';
+
+const GlobalStyles = createGlobalStyle`
+  ${reset}
+`;
 
 function App() {
   return (
     <>
+      <GlobalStyles />
       <Toggle />
     </>
   );
